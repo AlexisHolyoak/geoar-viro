f5f82abcff564cb67d1bb1e69948d998ae20bb9d
Alexis Jesus Peralta Holyoak <peraltaholyoak.aj@gmail.com>
2022-04-28 16:16:26 -0500

    skull model

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import {
   ViroARScene,
   ViroText,
@@ -8,6 +8,8 @@ import {
   ViroBox,
   ViroMaterials,
   ViroAnimations,
+  Viro3DObject,
+  ViroAmbientLight,
 } from '@viro-community/react-viro';
 
 const InitialScene = () => {
@@ -26,29 +28,58 @@ const InitialScene = () => {
   });
   return (
     <ViroARScene>
-      <ViroBox
-        height={2}
-        length={2}
-        width={2}
-        position={[0, -1, -1]}
-        scale={[0.2, 0.2, 0.2]}
-        materials={['wood']}
-        animation={{ name: 'rotate', loop: true, run: true }}
-      />
+      <ViroAmbientLight color="#ffffff" />
+      <Viro3DObject
+        source={require('./assets/skull/12140_Skull_v3_L2.obj')}
+        position={[0, 0, -5]}
+        scale={[0.05, 0.05, 0.05]}
+        rotation={[-45, 50, 40]}
+        type="OBJ"
+      ></Viro3DObject>
     </ViroARScene>
   );
 };
 
 export default () => {
+  const [object, setObject] = useState('skull');
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: InitialScene,
-      }}
-      style={{ flex: 1 }}
-    />
+    <View style={styles.mainView}>
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: InitialScene,
+        }}
+        viroAppProps={{ object: object }}
+        style={{ flex: 1 }}
+      />
+      <View style={styles.controlsView}>
+        <TouchableOpacity onPress={() => setObject('skull')}>
+          <Text style={styles.text}>Display Skull</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setObject('tv')}>
+          <Text style={styles.text}>Display TV</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
-var styles = StyleSheet.create({});
+var styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+  },
+  text: {
+    margin: 20,
+    backgroundColor: '#9d9d9d',
+    padding: 10,
+    fontWeight: 'bold',
+  },
+  controlsView: {
+    width: '100%',
+    height: 100,
+    backgroundColor: '#ffffff',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
