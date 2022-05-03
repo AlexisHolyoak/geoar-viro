fb88461b7cf23d7fed746287d5e5983f3c90c9e7
Alexis Jesus Peralta Holyoak <peraltaholyoak.aj@gmail.com>
2022-05-02 22:53:14 -0500

    geoar working

--- js/ArScene.tsx
+++ js/ArScene.tsx
@@ -11,6 +11,8 @@ import {
   Viro3DObject,
   ViroAmbientLight,
   ViroAnimations,
+  ViroFlexView,
+  ViroImage,
 } from '@viro-community/react-viro';
 import Geolocation from 'react-native-geolocation-service';
 import { requestMultiple, PERMISSIONS, RESULTS, Permission } from 'react-native-permissions';
@@ -20,6 +22,25 @@ const Toast = (message: any) => {
 const MAPS_API_KEY = 'AIzaSyCB2qjHgUAhxuzXygjEGeQBiBDpaQcmNfw';
 const PlacesAPIURL = (lat: any, lng: any) =>
   `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=50&key=${MAPS_API_KEY}`;
+const distanceBetweenPoints = (p1: any, p2: any) => {
+  if (!p1 || !p2) {
+    return 0;
+  }
+
+  var R = 6371; // Radius of the Earth in km
+  var dLat = ((p2.latitude - p1.latitude) * Math.PI) / 180;
+  var dLon = ((p2.longitude - p1.longitude) * Math.PI) / 180;
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((p1.latitude * Math.PI) / 180) *
+      Math.cos((p2.latitude * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+};
+
 export default class ARScene extends React.Component<any, any> {
   listener: any;
   constructor(props: any) {
@@ -107,21 +128,71 @@ export default class ARScene extends React.Component<any, any> {
         console.error(error);
       });
   };
+  transformGpsToAR = (lat: any, lng: any) => {
+    const isAndroid = Platform.OS === 'android';
+    const latObj = lat;
+    const longObj = lng;
+    const latMobile = this.state.location.latitude;
+    const longMobile = this.state.location.longitude;
+
+    const deviceObjPoint = this.latLongToMerc(latObj, longObj);
+    const mobilePoint = this.latLongToMerc(latMobile, longMobile);
+    const objDeltaY = deviceObjPoint.y - mobilePoint.y;
+    const objDeltaX = deviceObjPoint.x - mobilePoint.x;
+
+    if (isAndroid) {
+      let degree = this.state.compassHeading;
+      let angleRadian = (degree * Math.PI) / 180;
+      let newObjX = objDeltaX * Math.cos(angleRadian) - objDeltaY * Math.sin(angleRadian);
+      let newObjY = objDeltaX * Math.sin(angleRadian) + objDeltaY * Math.cos(angleRadian);
+      return { x: newObjX, z: -newObjY };
+    }
+
+    return { x: objDeltaX, z: -objDeltaY };
+  };
+  latLongToMerc = (latDeg: any, longDeg: any) => {
+    // From: https://gist.github.com/scaraveos/5409402
+    const longRad = (longDeg / 180.0) * Math.PI;
+    const latRad = (latDeg / 180.0) * Math.PI;
+    const smA = 6378137.0;
+    const xmeters = smA * longRad;
+    const ymeters = smA * Math.log((Math.sin(latRad) + 1) / Math.cos(latRad));
+    return { x: xmeters, y: ymeters };
+  };
   placeArObjects = () => {
     if (this.state.nearbyPlaces.length == 0) {
       return undefined;
     }
     const ARTags = this.state.nearbyPlaces.map((item: any) => {
-      console.log(item);
+      const coords = this.transformGpsToAR(item.lat, item.lng);
+      const scale = Math.abs(Math.round(coords.z / 15));
+      const distance = distanceBetweenPoints(this.state.location, {
+        latitude: item.lat,
+        longitude: item.lng,
+      });
+      console.log('Calling placeArObjects: distance -> ' + distance);
+      return (
+        <ViroNode
+          key={item.id}
+          scale={[scale, scale, scale]}
+          rotation={[0, 0, 0]}
+          position={[coords.x, 0, coords.z]}
+        >
+          <ViroFlexView style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <ViroText width={4} height={0.5} text={item.title} style={styles.helloWorldTextStyle} />
+            <ViroText
+              width={4}
+              height={0.5}
+              text={`${Number(distance).toFixed(2)} km`}
+              style={styles.helloWorldTextStyle}
+              position={[0, -0.75, 0]}
+            />
+            <ViroImage width={1} height={1} source={{ uri: item.icon }} position={[0, -1.5, 0]} />
+          </ViroFlexView>
+        </ViroNode>
+      );
     });
-    return (
-      <ViroText
-        text={this.state.text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    );
+    return ARTags;
   };
   render() {
     return (
