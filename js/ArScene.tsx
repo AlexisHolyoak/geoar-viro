2873b860e18f61274092228e35b88e077c9b6b01
Alexis Jesus Peralta Holyoak <peraltaholyoak.aj@gmail.com>
2022-05-02 21:44:42 -0500

    camera & location W

--- js/ArScene.tsx
+++ js/ArScene.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, ToastAndroid } from 'react-native';
+import { StyleSheet, ToastAndroid, Platform } from 'react-native';
 import {
   ViroARScene,
   ViroQuad,
@@ -12,10 +12,16 @@ import {
   ViroAmbientLight,
   ViroAnimations,
 } from '@viro-community/react-viro';
+import Geolocation from '@react-native-community/geolocation';
+import { requestMultiple, PERMISSIONS, RESULTS, Permission } from 'react-native-permissions';
 const Toast = (message: any) => {
   ToastAndroid.showWithGravityAndOffset(message, ToastAndroid.LONG, ToastAndroid.BOTTOM, 25, 50);
 };
+const MAPS_API_KEY = 'AIzaSyCB2qjHgUAhxuzXygjEGeQBiBDpaQcmNfw';
+const PlacesAPIURL = (lat: any, lng: any) =>
+  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=50&key=${MAPS_API_KEY}`;
 export default class ARScene extends React.Component<any, any> {
+  listener: any;
   constructor(props: any) {
     super(props);
 
@@ -32,17 +38,34 @@ export default class ARScene extends React.Component<any, any> {
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this.listener = undefined;
   }
-
+  componentDidMount() {
+    requestMultiple([PERMISSIONS.ANDROID.CAMERA, PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]).then(
+      (statuses) => {
+        console.log('Camera', statuses[PERMISSIONS.ANDROID.CAMERA]);
+        console.log('Location', statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]);
+        this.setState({
+          locationReady: statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] === RESULTS.GRANTED,
+          cameraReady: statuses[PERMISSIONS.ANDROID.CAMERA] === RESULTS.GRANTED,
+        });
+      }
+    );
+  }
+  placeArObjects = () => {
+    return (
+      <ViroText
+        text={this.state.text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    );
+  };
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
-        <ViroText
-          text={this.state.text}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={styles.helloWorldTextStyle}
-        />
+        {this.state.locationReady && this.state.cameraReady && this.placeArObjects()}
       </ViroARScene>
     );
   }
